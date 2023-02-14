5cb9bd3acbb77b7bed7acf6bbd1add5e6ddd3a9d
Dexer <73297572+DexerBR@users.noreply.github.com>
2023-02-14 01:00:22 -0300

    wip

---
+++ kivy_ios/recipes/av/__init__.py
@@ -0,0 +1,26 @@
+from pythonforandroid.toolchain import Recipe
+from pythonforandroid.recipe import CythonRecipe
+from os.path import join
+
+
+class PyAVRecipe(CythonRecipe):
+
+    name = "av"
+    library = "av.a"
+    version = "v10.0.0"
+    url = "https://github.com/PyAV-Org/PyAV/archive/v{version}.zip"
+
+    depends = ["python3", "cython", "ffmpeg", "av_codecs"]
+    opt_depends = ["openssl"]
+    pre_build_ext = True
+
+    def get_recipe_env(self, arch, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch)
+
+        build_dir = join(self.ctx.dist_dir, "include", arch.arch, "ffmpeg")
+        self.setup_extra_args = ["--ffmpeg-dir={}".format(build_dir)]
+
+        return env
+
+
+recipe = PyAVRecipe()
