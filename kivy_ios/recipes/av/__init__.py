2437aa3e53ca552ed0bd0e850496f172b3326a41
Dexer <73297572+DexerBR@users.noreply.github.com>
2023-02-15 14:01:37 -0300

    attempt 5

--- kivy_ios/recipes/av/__init__.py
+++ kivy_ios/recipes/av/__init__.py
@@ -1,5 +1,5 @@
-from kivy_ios.toolchain import CythonRecipe, shprint
-import sh
+from kivy_ios.toolchain import CythonRecipe
+# import sh
 
 
 class PyAVRecipe(CythonRecipe):
@@ -12,27 +12,27 @@ class PyAVRecipe(CythonRecipe):
     depends = ["python3", "ffmpeg"]
     opt_depends = ["openssl"]
     hostpython_prerequisites = ["Cython"]
-    cythonize = False
-    pre_build_ext = False
+    # cythonize = False
+    # pre_build_ext = False
 
     def get_recipe_env(self, arch, with_flags_in_cc=True):
         env = super().get_recipe_env(arch)
-        return env
-
-    def build_arch(self, arch):
-        build_env = self.get_recipe_env(arch)
-        hostpython3 = sh.Command(self.ctx.hostpython)
         build_dir = self.get_recipe('ffmpeg', self.ctx).get_build_dir(
             arch.arch
         )
-        shprint(
-            hostpython3,
-            "setup.py",
-            "build",
-            "--ffmpeg-dir={}".format(build_dir),
-            _env=build_env,
-        )
-        self.biglink()
+        env["--ffmpeg-dir={}".format(build_dir)]
+        return env
+
+    # def build_arch(self, arch):
+    #     hostpython3 = sh.Command(self.ctx.hostpython)
+    #     shprint(
+    #         hostpython3,
+    #         "setup.py",
+    #         "build",
+    #         "--ffmpeg-dir={}".format(build_dir),
+    #         _env=build_env,
+    #     )
+    #     self.biglink()
 
 
 recipe = PyAVRecipe()
