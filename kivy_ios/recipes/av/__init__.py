521ba78e4a08a2051c7215fb2f41c0c2306ac8a2
Dexer <73297572+DexerBR@users.noreply.github.com>
2023-03-06 23:05:46 -0300

    attempt 13

--- kivy_ios/recipes/av/__init__.py
+++ kivy_ios/recipes/av/__init__.py
@@ -5,6 +5,7 @@ import sh
 class PyAVRecipe(CythonRecipe):
 
     name = "av"
+    library = "av.a"
     version = "10.0.0"
     url = "https://github.com/PyAV-Org/PyAV/archive/v{version}.zip"
 
@@ -17,6 +18,10 @@ class PyAVRecipe(CythonRecipe):
 
     def get_recipe_env(self, arch, with_flags_in_cc=True):
         env = super().get_recipe_env(arch)
+        build_dir = self.get_recipe("ffmpeg", self.ctx).get_build_dir(
+            arch.arch
+        )
+        env["--ffmpeg-dir"] = build_dir
         return env
 
     def build_arch(self, arch):
@@ -29,6 +34,7 @@ class PyAVRecipe(CythonRecipe):
             "setup.py",
             "build",
             "--ffmpeg-dir={}".format(ffmpeg_dir),
+            # _env=build_env,
         )
         self.biglink()
 
