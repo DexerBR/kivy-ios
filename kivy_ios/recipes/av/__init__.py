d133bd7dc955c37366fa8dc7c7e18955ab0c8cc4
Dexer <73297572+DexerBR@users.noreply.github.com>
2024-10-02 17:18:20 -0300

    wip

---
+++ kivy_ios/recipes/av/__init__.py
@@ -0,0 +1,37 @@
+from kivy_ios.toolchain import CythonRecipe, shprint
+import sh
+
+
+class PyAVRecipe(CythonRecipe):
+
+    name = "av"
+    library = "av.a"
+    version = "10.0.0"
+    url = "https://github.com/PyAV-Org/PyAV/archive/v{version}.zip"
+
+    depends = ["python3", "ffmpeg", "hostpython3"]
+    opt_depends = ["openssl"]
+    hostpython_prerequisites = ["Cython"]
+
+    cythonize = False
+    pre_build_ext = False
+
+    def get_recipe_env(self, arch, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch)
+        return env
+
+    def build_arch(self, arch):
+        hostpython3 = sh.Command(self.ctx.hostpython)
+        ffmpeg_dir = self.get_recipe("ffmpeg", self.ctx).get_build_dir(
+            arch.arch
+        )
+        shprint(
+            hostpython3,
+            "setup.py",
+            "build",
+            "--ffmpeg-dir={}".format(ffmpeg_dir),
+        )
+        self.biglink()
+
+
+recipe = PyAVRecipe()
